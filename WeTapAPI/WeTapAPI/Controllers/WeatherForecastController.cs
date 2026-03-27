05838a7100bb7312c5bb8f3297d82e3d73f78d36
Petro <petrotymchuck@gmail.com>
2026-03-27 06:45:58 +0200

    add base project structure

--- WeTapAPI/WeTapAPI/Controllers/WeatherForecastController.cs
+++ WeTapAPI/WeTapAPI/Controllers/WeatherForecastController.cs
@@ -12,17 +12,9 @@ namespace WeTapAPI.Controllers
         ];
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            //logger.LogInformation("---Get Wheather---");
-            //logger.LogError("---Get Wheather---");
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Summaries;
         }
     }
 }
