f69328b2d81d281c6f2d058788c097c61c116f89
Petro <petrotymchuck@gmail.com>
2026-03-27 07:36:24 +0200

    add genre entity

---
+++ WeTapAPI/Domain/Entities/GenreEntity.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities;
+
+[Table("tbl_genres")]
+public class GenreEntity : BaseEntity<long>
+{
+    [StringLength(255)]
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(255)]
+    public string Slug { get; set; } = string.Empty;
+
+    [StringLength(255)]
+    public string? Image { get; set; }
+}
