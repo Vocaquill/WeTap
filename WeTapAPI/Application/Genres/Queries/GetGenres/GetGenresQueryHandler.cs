5c7c7b4583ec7f99de2022aae014f176bab44ab9
Petro <petrotymchuck@gmail.com>
2026-03-28 08:04:39 +0200

    fix handler

--- WeTapAPI/Application/Genres/Queries/GetGenres/GetGenresQueryHandler.cs
+++ WeTapAPI/Application/Genres/Queries/GetGenres/GetGenresQueryHandler.cs
@@ -6,23 +6,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Genres.Queries.GetGenres;
 
-public class GetGenresQueryHandler : IRequestHandler<GetGenresQuery, IEnumerable<GenreItemModel>>
+public class GetGenresQueryHandler(AppDbContext context, IMapper mapper) : IRequestHandler<GetGenresQuery, IEnumerable<GenreItemModel>>
 {
-    private readonly AppDbContext _context;
-    private readonly IMapper _mapper;
-
-    public GetGenresQueryHandler(AppDbContext context, IMapper mapper)
-    {
-        _context = context;
-        _mapper = mapper;
-    }
-
     public async Task<IEnumerable<GenreItemModel>> Handle(GetGenresQuery request, CancellationToken cancellationToken)
     {
-        var genres = await _context.Genres
+        var genres = await context.Genres
             .AsNoTracking()
             .ToListAsync(cancellationToken);
             
-        return _mapper.Map<IEnumerable<GenreItemModel>>(genres);
+        return mapper.Map<IEnumerable<GenreItemModel>>(genres);
     }
 }
