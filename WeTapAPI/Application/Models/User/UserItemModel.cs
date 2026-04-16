826c824cf08c538f3608129f77542e440ffb34fc
DmitroKozachok <149708709+DmitroKozachok@users.noreply.github.com>
2026-04-16 10:20:04 +0300

    Add user management models: Delete, Edit, and Item
    Introduced three new models under Application.Models.User:
    - UserDeleteModel for user deletion by Id.
    - UserEditModel for editing user details, including roles and image.
    - UserItemModel for representing user data, login types, and roles.
    These models support user listing, editing, and deletion features.

---
+++ WeTapAPI/Application/Models/User/UserItemModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Models.User;
+
+public class UserItemModel
+{
+    public long Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public bool IsLoginGoogle { get; set; } = false;
+    public bool IsLoginPassword { get; set; } = false;
+    public List<string> Roles { get; set; } = new();
+    public List<string> LoginTypes { get; set; } = new();
+}
