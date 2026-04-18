8e2ad696cb76946687cfcfa52ee63c81a3a27e43
Petro <petrotymchuck@gmail.com>
2026-04-18 06:31:58 +0300

    working test video uploading

--- WeTapSite/wetap-web/src/store/index.ts
+++ WeTapSite/wetap-web/src/store/index.ts
@@ -5,6 +5,8 @@ import {type TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import {apiGenres} from "../services/api/apiGenres.ts";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import {apiAccount} from "../services/api/apiAccount.ts";
+import {apiVideos} from "../services/api/apiVideos.ts";
+
 import authReducer from "./slices/authSlice.ts";
 
 export const store = configureStore({
@@ -14,6 +16,8 @@ export const store = configureStore({
         [apiMovies.reducerPath]: apiMovies.reducer,
         [apiGenres.reducerPath]: apiGenres.reducer,
         [apiAccount.reducerPath]: apiAccount.reducer,
+        [apiVideos.reducerPath]: apiVideos.reducer,
+
         //[apiUser.reducerPath]: apiUser.reducer,
     },
 
@@ -21,7 +25,9 @@ export const store = configureStore({
         getDefaultMiddleware().concat(
             apiMovies.middleware,
             apiGenres.middleware,
-            apiAccount.middleware
+            apiAccount.middleware,
+            apiVideos.middleware
+
             //apiUser.middleware
         )
 });
