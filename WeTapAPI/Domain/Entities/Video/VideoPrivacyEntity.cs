b7915773580b59e0eb34e4cb3f51cf54a94c52c4
Petro <petrotymchuck@gmail.com>
2026-04-19 06:53:19 +0300

    add privacy entity

---
+++ WeTapAPI/Domain/Entities/Video/VideoPrivacyEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.Video;
+
+public class VideoPrivacyEntity : BaseEntity<long>
+{
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50)]
+    public string SystemCode { get; set; } = string.Empty;
+
+    public virtual ICollection<VideoEntity>? Videos { get; set; } = new List<VideoEntity>();
+}
