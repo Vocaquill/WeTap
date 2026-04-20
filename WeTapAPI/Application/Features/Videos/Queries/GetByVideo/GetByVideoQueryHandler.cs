8b2e7bb5981c8e29707953856bebcbafd0e115b1
Petro <petrotymchuck@gmail.com>
2026-04-20 06:53:15 +0300

    add view count logic

--- WeTapAPI/Application/Features/Videos/Queries/GetByVideo/GetByVideoQueryHandler.cs
+++ WeTapAPI/Application/Features/Videos/Queries/GetByVideo/GetByVideoQueryHandler.cs
@@ -14,18 +14,28 @@ public class GetByVideoQueryHandler(IGenericRepository<VideoEntity, long> repo, 
     public async Task<VideoItemModel> Handle(GetByVideoQuery request, CancellationToken cancellationToken)
     {
         VideoItemModel model = new VideoItemModel();
+        VideoEntity entity = new VideoEntity();
         if (request.Model.Id != null) 
         {
-            var entity = await repo.GetByIdAsync(request.Model.Id.Value);
+            entity = await repo.GetByIdAsync(request.Model.Id.Value);
+
+            if (entity == null)
+                throw new Exception($"Відео з id {request.Model.Id.Value} не знайдено");
 
             model = mapper.Map<VideoItemModel>(entity);
         }
         else if (request.Model.Slug != null) 
         {
-            var entity = await repo.AsQurable().Where(x => x.Slug == request.Model.Slug).FirstAsync();
-            
+            entity = await repo.AsQurable().Where(x => x.Slug == request.Model.Slug).FirstAsync();
+
+            if (entity == null)
+                throw new Exception($"Відео з slug {request.Model.Slug} не знайдено");
+
             model = mapper.Map<VideoItemModel>(entity);
         }
+
+        entity.ViewCount++;
+        await repo.UpdateAsync(entity);
 
         return model;
     }
