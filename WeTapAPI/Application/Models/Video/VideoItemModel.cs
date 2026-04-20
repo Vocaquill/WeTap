fe337e7bf0eac68a5f73fe966a30608c12f531ac
Petro <petrotymchuck@gmail.com>
2026-04-20 06:54:17 +0300

    Add views to video model

--- WeTapAPI/Application/Models/Video/VideoItemModel.cs
+++ WeTapAPI/Application/Models/Video/VideoItemModel.cs
@@ -9,6 +9,7 @@ public class VideoItemModel
     public string Title { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
     public string? Description { get; set; }
+    public long ViewCount { get; set; }
 
     public string? Image { get; set; }
     public string? Video { get; set; }
