75b8cb7a5d414ad12b994426bdc144fe878c06e1
Petro <petrotymchuck@gmail.com>
2026-04-23 07:47:55 +0300

    add video service

--- WeTapSite/wetap-web/src/store/index.ts
+++ WeTapSite/wetap-web/src/store/index.ts
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import genresReducer from './slices/genresSlice';
-import {apiMovies} from "../services/api/apiMovies.ts";
+import {apiVideos} from "../services/api/apiVideos.ts";
+
 import {type TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import {apiGenres} from "../services/api/apiGenres.ts";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import {apiAccount} from "../services/api/apiAccount.ts";
-import {apiVideos} from "../services/api/apiVideos.ts";
 import {apiTags} from "../services/api/apiTags.ts";
 import {apiLanguages} from "../services/api/apiLanguages.ts";
 
@@ -16,10 +16,10 @@ export const store = configureStore({
     reducer: {
         auth: authReducer,
         // genres: genresReducer,
-        [apiMovies.reducerPath]: apiMovies.reducer,
+        [apiVideos.reducerPath]: apiVideos.reducer,
+
         [apiGenres.reducerPath]: apiGenres.reducer,
         [apiAccount.reducerPath]: apiAccount.reducer,
-        [apiVideos.reducerPath]: apiVideos.reducer,
         [apiTags.reducerPath]: apiTags.reducer,
         [apiLanguages.reducerPath]: apiLanguages.reducer,
 
@@ -29,10 +29,10 @@ export const store = configureStore({
 
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(
-            apiMovies.middleware,
+            apiVideos.middleware,
+
             apiGenres.middleware,
             apiAccount.middleware,
-            apiVideos.middleware,
             apiTags.middleware,
             apiLanguages.middleware
 
