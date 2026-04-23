3f68624fecd4a0c8c055f8e9ffd8caf62deaa74a
Petro <petrotymchuck@gmail.com>
2026-04-23 07:46:19 +0300

    add video interface

---
+++ WeTapSite/wetap-web/src/types/Video/IVideoCreateRequest.ts
@@ -0,0 +1,10 @@
+export interface IVideoCreateRequest {
+    title: string;
+    slug: string;
+    description: string;
+    genreIds?: number[];
+    tagIds?: number[];
+    image?: File;
+    video?: File;
+    languageId: number;
+}
