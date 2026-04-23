a7e015bdd8e6229bc599c14875fe6612cc68fb1c
Petro <petrotymchuck@gmail.com>
2026-04-23 07:08:54 +0300

    fix language delete

--- WeTapAPI/Application/Features/Languages/Commands/DeleteLanguage/DeleteLanguageCommand.cs
+++ WeTapAPI/Application/Features/Languages/Commands/DeleteLanguage/DeleteLanguageCommand.cs
@@ -4,4 +4,4 @@ using MediatR;
 namespace Application.Features.Languages.Commands.DeleteLanguage;
 
 public record DeleteLanguageCommand(LanguageDeleteModel Model)
-    : IRequest<IEnumerable<LanguageItemModel>>;
+    : IRequest;
