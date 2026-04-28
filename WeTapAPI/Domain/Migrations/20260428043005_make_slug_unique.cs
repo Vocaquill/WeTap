b118e39244c4d71a64256fab88d0c8b7ff1cda50
Petro <petrotymchuck@gmail.com>
2026-04-28 07:31:19 +0300

    create unique slug migrations

---
+++ WeTapAPI/Domain/Migrations/20260428043005_make_slug_unique.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class make_slug_unique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_videos_Slug",
+                table: "tbl_videos",
+                column: "Slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_video_languages_LanguageCode",
+                table: "tbl_video_languages",
+                column: "LanguageCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_tags_Name",
+                table: "tbl_tags",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_tags_Slug",
+                table: "tbl_tags",
+                column: "Slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbl_genres_Slug",
+                table: "tbl_genres",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_tbl_videos_Slug",
+                table: "tbl_videos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tbl_video_languages_LanguageCode",
+                table: "tbl_video_languages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tbl_tags_Name",
+                table: "tbl_tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tbl_tags_Slug",
+                table: "tbl_tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tbl_genres_Slug",
+                table: "tbl_genres");
+        }
+    }
+}
