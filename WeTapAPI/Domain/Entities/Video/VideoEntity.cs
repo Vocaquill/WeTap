fc551321a3c0089be54c13a99f7ee684eaf96b6e
Petro <petrotymchuck@gmail.com>
2026-05-07 06:51:03 +0300

    add video channel rel migration

--- WeTapAPI/Domain/Entities/Video/VideoEntity.cs
+++ WeTapAPI/Domain/Entities/Video/VideoEntity.cs
@@ -51,4 +51,5 @@ public class VideoEntity : BaseEntity<long>
 
     public virtual ICollection<VideoGenreEntity>? VideoGenres { get; set; } = new List<VideoGenreEntity>();
     public virtual ICollection<VideoTagEntity>? VideoTags { get; set; } = new List<VideoTagEntity>();
+    public virtual ICollection<CommentsEntity> Comments { get; set; } = new List<CommentsEntity>();
 }
