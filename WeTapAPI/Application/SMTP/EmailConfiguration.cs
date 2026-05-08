9c3062e2a83700aad8de8c66e20c00bb334b99a5
DmitroKozachok <149708709+DmitroKozachok@users.noreply.github.com>
2026-05-08 10:58:14 +0300

    Add SMTP email service with MailKit and configuration
    Implemented email sending via new SmtpService using MailKit and MimeKit. Introduced ISmtpService interface, EmailMessage model, and EmailConfiguration for SMTP settings. Added required NuGet packages.

---
+++ WeTapAPI/Application/SMTP/EmailConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.SMTP;
+
+public class EmailConfiguration
+{
+    public const string From = "sendmessage1@ukr.net";
+    public const string SmtpServer = "smtp.ukr.net";
+    public const int Port = 2525;
+    public const string UserName = "sendmessage1@ukr.net";
+    public const string Password = "yhxz0Uq3NJHqBELt";
+}
---
+++ WeTapAPI/Application/SMTP/EmailMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.SMTP;
+
+public class EmailMessage
+{
+    public string Subject { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public string To { get; set; } = string.Empty;
+}
