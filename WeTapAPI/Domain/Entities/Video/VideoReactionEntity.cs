4cf520557cd4aadf03d7d47f9bf5ff7d69de09cb
Petro <petrotymchuck@gmail.com>
2026-05-14 06:57:24 +0300

    add reaction entities

---
+++ WeTapAPI/Domain/Entities/Video/VideoReactionEntity.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Identity;
+
+namespace Domain.Entities.Video;
+
+public class VideoReactionEntity : BaseEntity<long>
+{
+    public long VideoId { get; set; }
+    public VideoEntity Video { get; set; } = null!;
+
+    public long UserId { get; set; }
+    public UserEntity User { get; set; } = null!;
+
+    public bool IsLike { get; set; }
+}
