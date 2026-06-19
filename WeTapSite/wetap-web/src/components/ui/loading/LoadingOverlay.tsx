634b53f29e1f056888befd0dceae09e7090493ac
Petro <petrotymchuck@gmail.com>
2026-06-19 13:36:06 +0300

    add loading overlay to auth pages

--- WeTapSite/wetap-web/src/components/ui/loading/LoadingOverlay.tsx
+++ WeTapSite/wetap-web/src/components/ui/loading/LoadingOverlay.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const LoadingOverlay: React.FC = () => {
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-md">
